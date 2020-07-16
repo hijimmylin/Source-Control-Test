c3a1ca0a7e177c0876a3d2004c39bda0321c0d6b
James Lin <jameslin83@gmail.com>
2020-07-16 13:41:17 -0700

    Initial Commit

---
+++ Source Control Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Source Control Test
+//
+//  Created by James Lin on 7/16/20.
+//  Copyright © 2020 Jimmy Press Play. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
